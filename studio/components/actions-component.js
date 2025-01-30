cbb493427956438adaed996b53bd31046c4c4405
Will Smith <minkcv@gmail.com>
2025-01-30 09:17:30 -0700

    fix studio missing rom for assemble and run

--- studio/components/actions-component.js
+++ studio/components/actions-component.js
@@ -3,7 +3,7 @@ layout.registerComponent( 'actionsComponent', function(container, componentState
     `<div class='actions-menu' id='actions-menu'>
         <button type='button' onclick='asmEditor.setValue(compile(vaporEditor.getValue())); loadProgram(programStringToBinary(assemble(asmEditor.getValue())), getRom())'>Compile, Assemble, and Run</button>
         <button type='button' onclick='asmEditor.setValue(compile(vaporEditor.getValue()));'>Compile</button>
-        <button type='button' onclick='loadProgram(programStringToBinary(assemble(asmEditor.getValue())))'>Assemble and Run</button>
+        <button type='button' onclick='loadProgram(programStringToBinary(assemble(asmEditor.getValue())), getRom())'>Assemble and Run</button>
         <br><hr>
         Upload BIN:  <input type="file" id="upload_bin" multiple='false' onchange='uploadBin(this.files)'><br>
         Select BIN:  <select id='bin_select'>
